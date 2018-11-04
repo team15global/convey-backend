8a53e352ff025dfc218dbb1813a0dfc2c60003e4
TJ Allen <no.trbl.2.u@gmail.com>
2018-11-04 03:20:41 -0500

    Added Another Conditions db

--- tmpDB/testTraversal.js
+++ tmpDB/testTraversal.js
@@ -1,8 +1,8 @@
 const allStrains = require('./cannabis.json')
+const allConditions = require('./conditions.json')
 
+// const test = allConditions.filter(eaCond => allStrains.map(eaStrain => eaStrain.strain === eaCond.muscleSpasms ))
+// console.log(allConditions.map(ea => ea.strains))
 
-// const nameArray = allStrains.map(ea => ea.Strain)
-// const highestRating = allStrains.filter(ea => parseFloat(ea.Rating) === 5.0)
-// const totalAmt = allStrains.length;
-
-// console.log(highestRating.map(ea => ea.Strain))
+const muscleSpasmsArray = allConditions.map(ea => ea.strains);
+console.log(muscleSpasmsArray)
