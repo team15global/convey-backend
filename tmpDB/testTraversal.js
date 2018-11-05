745276d701dfd4cc5292aee1ff8a8aaee7f874ab
TJ Allen <no.trbl.2.u@gmail.com>
2018-11-05 12:02:51 -0500

    Cleaned up folder

--- tmpDB/testTraversal.js
+++
@@ -1,8 +0,0 @@
-const allStrains = require('./cannabis.json')
-const allConditions = require('./conditions.json')
-
-// const test = allConditions.filter(eaCond => allStrains.map(eaStrain => eaStrain.strain === eaCond.muscleSpasms ))
-// console.log(allConditions.map(ea => ea.strains))
-
-const muscleSpasmsArray = allConditions.map(ea => ea.strains);
-console.log(muscleSpasmsArray)
--- tmpDB/xxx.js
+++
@@ -1,9 +0,0 @@
-const allConditions = require('./conditionsDec.json')
-
-
-const flatten = arr => [].concat(...arr);
-const flatten2 = arr => [...arr]
-
-const newList = flatten(allConditions);
-
-// require("fs").writeFileSync("./conditions2.json", JSON.stringify(newList, null, 2), 'utf8')
