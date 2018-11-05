657f545fa765580f03d6c9ebdba8030fbce7e309
TJ Allen <no.trbl.2.u@gmail.com>
2018-11-05 12:01:35 -0500

    Created working Query functions to assist in traversal of DB

--- tmpDB/dbFunctions.js
+++ tmpDB/dbFunctions.js
@@ -1,35 +1,20 @@
 const allStrains = require('./data/cannabis.json')
 const allConditions = require('./data/conditionsDec.json')
 
-// Destructoring options -> I could destructure all of them
-const {
-  glaucoma
-} = allConditions;
 
-// Query Function for condition
+// Query Functions for condition and strain
+
+// condition :: String -> Array
+// returns: Array of strains that cure condition
 const condition = name => allConditions.find(condition => condition.hasOwnProperty(name))[name]
 
-const keys = obj => Object.keys(obj);
-// 
+// strain :: String -> Object
+// returns Object with information on strain
+// keys -> "type", "rating", "effects", "flavor", "description"
+const strain = name => allStrains.filter((ea) => ea.strain === name )[0]
 
 
-// 1. Map over the array
-// 2. chop off head
-// 3. 
-
-
-// const strainInfo = name => {
-//   let keys = Object.keys(name).pop()
-//   allStrains   
-//     .map(ea)
-//     console.log(test)
-// }
-// strainInfo(keys)
-
-// strain('1024') // {"Type": 'hybrid', ...}
-
-// Example Query
-const eyePressure = condition("eyePressure")
-
-
-exports.condition = condition;
+module.exports = {
+  condition,
+  strain
+};
